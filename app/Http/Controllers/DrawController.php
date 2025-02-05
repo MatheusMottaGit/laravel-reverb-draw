d6d1dfef2cf6ab4a08a75a6c1512c19f24db85e0
Matheus <matheusprofess@gmail.com>
2025-02-05 12:20:58 -0300

    joining the draw is actually working

--- app/Http/Controllers/DrawController.php
+++ app/Http/Controllers/DrawController.php
@@ -4,30 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Events\UserJoined;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class DrawController extends Controller
 {
-    private $participants = [];
-
     public function enter(Request $request) {
         $nickname = $request->input('nickname');
 
-        if (in_array($nickname, $this->participants)) {
+        $participants = Cache::get('participants', []);
+
+        if (in_array($nickname, $participants)) {
             return response()->json(['message' => "This nickname is already in the room."], 400);
         }
 
-        $isAdmin = in_array("admin123", $this->participants);
+        $isAdmin = $nickname === "admin123";
 
-        array_push($this->participants, $nickname);
+        $participants[] = $nickname;
+        
+        Cache::put('participants', $participants);
 
-        broadcast(new UserJoined($nickname, $this->participants));
+        broadcast(new UserJoined($nickname, $participants));
 
         return response()->json([
-            'message' => $nickname . "joined the room."
+            'message' => "$nickname joined the room!" . ($isAdmin ? "(Administrador)." : "")
         ]);
     }
 
     public function start() {
-        
+        $participants = Cache::get('participants');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,5 @@
 use App\Http\Controllers\DrawController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/enter', [DrawController::class, 'enter']);
+Route::post('/start', [DrawController::class, 'start']);
